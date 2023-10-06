27d26c5bc2bc5c9ceec13b8212e5a37573c46ca2
Vic De Hondt <vic.dehondt@gmail.com>
2023-10-06 13:29:06 +0200

    Everything ready to start

--- frontend/cypress/e2e/spec.cy.ts
+++ frontend/cypress/e2e/spec.cy.ts
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Connect test', () => {
+  it('Connects to server', () => {
     cy.visit('http://localhost:3000')
+  })
+})
+describe('My First Test', () => {
+  it('Visits the Kitchen Sink', () => {
+    cy.visit('https://example.cypress.io')
   })
 })
