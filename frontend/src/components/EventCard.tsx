47fe1836b540167d2c149e8e6521caca1e3510b9
Vic De Hondt <vic.dehondt@gmail.com>
2023-10-16 21:41:32 +0200

    Added EventCard component

---
+++ frontend/src/components/EventCard.tsx
@@ -0,0 +1,54 @@
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "../styles/EventCard.module.css";
+import Link from "next/link";
+
+function EventCard() {
+  return (
+    <div className={styles.eventCard}>
+      <div className={styles.photo}>
+        <Image src="/photos/ariana.jpeg" width={120} height={120} alt="Performer" />
+      </div>
+      <div className={styles.event}>
+        <div className={styles.performance}>Ariana Grande</div>
+        <div className={styles.location}>
+          <Image src="/icons/location.png" width={18} height={21} alt="" />
+          <div>Lotto Arena</div>
+        </div>
+      </div>
+      <div className={styles.tickets}>
+        <div className={styles.photo}>
+          <Image src="/icons/crowd.png" width={18} height={17} alt="People attending" />
+        </div>
+        <div>560</div>
+      </div>
+      <div className={styles.info}>
+        <div className={styles.calendar}>
+          <div className={styles.photo}>
+            <Image src="/icons/date.png" width={38} height={41} alt="Date" />
+          </div>
+          <div className={styles.dateAndTime}>
+            <div className={styles.date}>9 oktober 2023</div>
+            <div className={styles.time}>18:00 &ndash; 22:00</div>
+          </div>
+        </div>
+        <div className={styles.price}>
+          <div className={styles.photo}>
+            <Image src="/icons/price.png" width={20} height={18} alt="Price" />
+          </div>
+          <div>100 &ndash; 200 EUR</div>
+        </div>
+      </div>
+      <div className={styles.wishlist}>
+        <Image src="/icons/heart.png" width={28} height={28} alt="Add to wishlist" />
+      </div>
+      <div className={styles.tags}>
+        <div className={styles.divider}></div>
+        <div className={styles.tag}>Pop</div>
+        <div className={styles.tag}>Alternative</div>
+      </div>
+    </div>
+  );
+}
+
+export default EventCard;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -5,6 +5,7 @@ import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 import Navbar from "../components/Navbar";
 import Searchbar from "../components/Searchbar";
+import EventCard from "../components/EventCard";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -37,49 +38,7 @@ export default function Home() {
               <h1>Events this week you may like</h1>
             </div>
             <div className={styles.eventCardContainer}>
-              <div className={styles.eventCard}>
-                <div className={styles.photo}>
-                  <Image src="/photos/ariana.jpeg" width={120} height={120} alt="Performer" />
-                </div>
-                <div className={styles.event}>
-                  <div className={styles.performance}>Ariana Grande</div>
-                  <div className={styles.location}>
-                    <Image src="/icons/location.png" width={18} height={21} alt="" />
-                    <div>Lotto Arena</div>
-                  </div>
-                </div>
-                <div className={styles.tickets}>
-                  <div className={styles.photo}>
-                    <Image src="/icons/crowd.png" width={18} height={17} alt="People attending" />
-                  </div>
-                  <div>560</div>
-                </div>
-                <div className={styles.info}>
-                  <div className={styles.calendar}>
-                    <div className={styles.photo}>
-                      <Image src="/icons/date.png" width={38} height={41} alt="Date" />
-                    </div>
-                    <div className={styles.dateAndTime}>
-                      <div className={styles.date}>9 oktober 2023</div>
-                      <div className={styles.time}>18:00 &ndash; 22:00</div>
-                    </div>
-                  </div>
-                  <div className={styles.price}>
-                    <div className={styles.photo}>
-                      <Image src="/icons/price.png" width={20} height={18} alt="Price" />
-                    </div>
-                    <div>100 &ndash; 200 EUR</div>
-                  </div>
-                </div>
-                <div className={styles.wishlist}>
-                  <Image src="/icons/heart.png" width={28} height={28} alt="Add to wishlist" />
-                </div>
-                <div className={styles.tags}>
-                  <div className={styles.divider}></div>
-                  <div className={styles.tag}>Pop</div>
-                  <div className={styles.tag}>Alternative</div>
-                </div>
-              </div>
+              <EventCard />
             </div>
           </div>
         </div>
