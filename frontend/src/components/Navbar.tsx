10a052f089b155fccd92d1246f71ddea16ce522a
Vic De Hondt <vic.dehondt@gmail.com>
2023-10-16 17:46:46 +0200

    Added stylesheet to navbar component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import styles from '../styles/Navbar.module.css'
 import Link from 'next/link'
 
 function Navbar() {
