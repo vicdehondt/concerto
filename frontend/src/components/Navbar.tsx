4fb6a81e9d0a5c8e1a9565434a6d88ec01ee3d25
Vic De Hondt <vic.dehondt@gmail.com>
2023-10-16 15:58:50 +0200

    Added navbar component

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
+
+function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.leftTopics}>
+        <Link href="/">Concerto</Link>
+        <div className={styles.search}>
+          <div className={styles.searchContainer}>
+            <div className={styles.searchBar}>
+              Search...
+            </div>
+          </div>
+        </div>
+        <div className={styles.addEvent}>
+          <Link href="/add-event">+</Link>
+        </div>
+      </div>
+      <div className={styles.rightTopics}>
+        <Link href="/friends">Friends</Link>
+        <Link href="/wishlist">Wishlist</Link>
+        <a>Notifications</a>
+        <Link href="/account">Account</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
