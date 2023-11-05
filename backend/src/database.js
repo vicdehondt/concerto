2f1db7b6b45bec2573bcc714c88406c5f0c117cc
reinoutcl <93149166+reinoutcl@users.noreply.github.com>
2023-11-05 20:40:01 +0100

    Storing/retrieving image
    - Created an image table in CustomerDB
    - Added function to store image in database.
    - Added function to retrieve image from database

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,10 +1,11 @@
 var sqlite3 = require('sqlite3').verbose()
+const fs = require('fs');
 
 // relative path - DOES NOT WORK because of compiling this file
 // const DBSOURCE = "SQLite/CustomerDB.db"
 
 // absolute path - FILL IN YOURSELF ex. "users/john/documents/webtech-project/backend/src/SQLite/CustomerDB.db"
-const DBSOURCE = ""
+const DBSOURCE = "/Users/reinout/Documents/GitHub/webtech-project/backend/src/SQLite/CustomerDB.db"
 
 let db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
@@ -49,6 +50,36 @@ function FindMail(Customerid) {
         });
     });
 }
+
+// testing to read an image from file
+const imageFilePath = '/Users/reinout/Documents/GitHub/webtech-project/backend/src/testimages/ariana.jpeg';
+const imageBuffer = fs.readFileSync(imageFilePath);
+
+function AddImage() {
+    db.run('INSERT INTO Images (imsrc) VALUES (?)',
+    [imageBuffer], (err)=> {
+        if (err) {
+            console.log("There was an error adding an image");
+            console.log(err.message)
+        } else {
+            console.log("Succesfully added image to database");
+        }
+    });
+}
+
+function ReadImage() {
+    db.get("SELECT imsrc FROM Images WHERE id = 1", (err, row) => {
+        if (!err && row) {
+            fs.writeFileSync('output_image.jpg', row.imsrc);
+        } else {
+            console.error("Error retrieving image:", err);
+          }
+    })
+}
+
+// AddImage()
+
+ReadImage()
 
 module.exports = {
     FindMail,
