6e67578895e5c7b96c67441c28c8df3cdbb3fe34
Vic De Hondt <vic.dehondt@gmail.com>
2023-11-15 21:22:40 +0100

    Fix serverside rendering to dynamic rendering using getStaticProps

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,46 +3,61 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import Navbar from "../components/Navbar";
 import EventCard from "../components/EventCard";
-import SideBar from "../components/SideBar"
-import { Nav } from "react-bootstrap";
-import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+import SideBar from "../components/SideBar";
+import { NextPageContext } from 'next';
 import { useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const environment = {
-  backendURL: "http://localhost:8080"
-}
-if (process.env.NODE_ENV == "production") {
-  environment.backendURL = "https://api.concerto.dehondt.dev"
+  backendURL: "http://localhost:8080",
+};
+
+if (process.env.NODE_ENV === "production") {
+  environment.backendURL = "https://api.concerto.dehondt.dev";
 }
 
 type Event = {
-  eventID: number
-  title: string
-  description: string
-  maxPeople: number
-  datetime: string
-  price: number
-  image: string
+  eventID: number;
+  title: string;
+  description: string;
+  maxPeople: number;
+  datetime: string;
+  price: number;
+  image: string;
+};
+
+Home.getInitialProps = async (ctx: NextPageContext) => {
+  const res = await fetch(environment.backendURL + "/events", {
+    mode: 'cors',
+    credentials: 'include',
+  });
+  const events = await res.json();
+  return { events };
+};
+
+interface HomeProps {
+  events: Array<Event>;
 }
 
-export const getServerSideProps = (async (context) => {
-  const res = await fetch(environment.backendURL + "/events")
-  const events = await res.json()
-  return { props: { events } }
-}) satisfies GetServerSideProps<{
-  events: Array<Event>
-}>
-
-export default function Home({events}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-
+export default function Home(props: HomeProps) {
   function showEvent(event: Event) {
-    return <EventCard key={event.eventID} eventId={event.eventID} title={event.title} location="Placeholder" amountAttending={event.maxPeople} dateAndTime={event.datetime} price={event.price} image={event.image} />
+    return (
+      <EventCard
+        key={event.eventID}
+        eventId={event.eventID}
+        title={event.title}
+        location="Placeholder"
+        amountAttending={event.maxPeople}
+        dateAndTime={event.datetime}
+        price={event.price}
+        image={event.image}
+      />
+    );
   }
 
   function showEvents() {
-    return events.map(showEvent);
+    return props.events.map(showEvent);
   }
 
   return (
@@ -55,17 +70,15 @@ export default function Home({events}: InferGetServerSidePropsType<typeof getSer
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
         <div className={[styles.page, styles.homePage].join(" ")}>
-          <SideBar type="event"/>
+          <SideBar type="event" />
           <div className={styles.pageContent}>
             <div className={styles.title}>
               <h1>Events this week you may like</h1>
             </div>
-            <div className={styles.eventCardContainer}>
-              {showEvents()}
-            </div>
+            <div className={styles.eventCardContainer}>{showEvents()}</div>
           </div>
         </div>
       </main>
     </>
   );
-}
+};
