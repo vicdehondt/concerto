650b09a69a005f59b3bf5c946f70c05618d846ef
Vic De Hondt <vic.dehondt@gmail.com>
2023-11-15 21:23:14 +0100

    Add form data for adding event

--- frontend/src/pages/add-event.tsx
+++ frontend/src/pages/add-event.tsx
@@ -1,67 +1,77 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { FormEvent } from 'react'
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+import { useRouter } from "next/router";
+import FriendInvites from "@/components/FriendInvite";
+import Banner from "@/components/Banner"
+import type { InferGetServerSidePropsType, GetServerSideProps } from 'next'
+// import Timetable from "/components/Timetable";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export default function AddEvent() {
+const environment = {
+  backendURL: "http://localhost:8080"
+}
+if (process.env.NODE_ENV == "production") {
+  environment.backendURL = "https://api.concerto.dehondt.dev"
+}
 
-  async function onSubmit(event: FormEvent<HTMLFormElement>) {
-    event.preventDefault()
+type Event = {
+  eventID: number
+  title: string
+  description: string
+  maxPeople: number
+  datetime: string
+  price: number
+  image: string
+}
 
-    const formData = new FormData(event.currentTarget)
-    const response = await fetch('http://localhost:8080/event/retrieve?id=1', {
-      method: 'GET',
-      // body: formData,
-    })
+export const getServerSideProps = (async (context) => {
+  const id = context.query.id
+  const res = await fetch(environment.backendURL + `/events/${id}`);
+  const event = await res.json();
+  return { props: { event } }
+}) satisfies GetServerSideProps<{
+  event: Event
+}>
 
-    // Handle response if necessary
-    const data = await response.json()
-    const imageData = data.image
-    console.log(data)
-    console.log(imageData)
-    // ...
-  }
+
+export default function AddEvent({event}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const router = useRouter();
+  const title = "Concerto | Add a concert" + router.query.concert;
 
   return (
     <>
       <Head>
-        <title>Concerto | Add an event</title>
-        <meta name="description" content="Add your event!" />
+        <title>Concerto | Add a concert</title>
+        <meta name="description" content="Add a concert." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-        <div className={[styles.page, styles.addEventPage].join(" ")}>
-        {/* <form action="http://localhost:8080/event/add" method="POST" enctype='multipart/form-data'> */}
-        <form onSubmit={onSubmit}>
-            {/* <div className={styles.inputGroup}>
-                <input type="text" name='title' id='title' required placeholder="Enter the title" />
+        <form className={[styles.page, styles.concertPage].join(" ")}>
+          <div className={styles.bannerContainer}>
+            <input id='banner' name='banner' type="file" required />
+          </div>
+          <div className={styles.descriptionContainer}>
+            <div className={styles.descriptionTitle}>
+              Description
             </div>
-            <div className={styles.inputGroup}>
-                <input type="text" name='description' id='description' required placeholder="Enter the description" />
+            <div className={styles.descriptionText}>
+              <textarea id='description' name='description' required />
             </div>
-            <div className={styles.inputGroup}>
-                <input type="text" name='datetime' id='datetime' required placeholder="Enter the date and time" />
-            </div>
-            <div className={styles.inputGroup}>
-                <input type="number" name='maxpeople' id='maxpeople' required placeholder="Enter the maximum amount of people" />
-            </div>
-            <div className={styles.inputGroup}>
-                <input type="number" name='price' id='price' required placeholder="Enter the ticket price" />
-            </div>
-            <div className={styles.inputGroup}>
-                <input type="number" name='eventid' id='eventid' required placeholder="Enter the event id" />
-            </div>
-            <div className={styles.inputGroup}>
-                <input id='image' name='image' type="file" required multiple />
-            </div> */}
-            <button className={styles.submitButton} type='submit'>Upload</button>
+          </div>
+          <div className={styles.timetableContainer}>
+            {/* <Timetable /> */}
+          </div>
+          <div className={styles.ratingContainer}>
+            Testing
+          </div>
+          <div className={styles.friendInviteContainer}>
+            <FriendInvites />
+          </div>
         </form>
-        </div>
       </main>
     </>
-  )
+  );
 }
--- frontend/src/pages/concerts/[id]/[concert].tsx
+++ frontend/src/pages/concerts/[id]/[concert].tsx
@@ -43,8 +43,7 @@ export default function Concert({event}: InferGetServerSidePropsType<typeof getS
   return (
     <>
       <Head>
-        {/* <title>{title}</title> */}
-        <title>yoooo</title>
+        <title>{title}</title>
         <meta name="description" content="Concert page." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
