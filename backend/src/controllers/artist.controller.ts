4fc064bbd5a6586ffd1dba6b863ed476fbf97ccf
reinoutcl <93149166+reinoutcl@users.noreply.github.com>
2023-11-24 18:16:25 +0100

    Safety check
    - Implemented check to prevent overloading musicbrainz api

--- backend/src/controllers/artist.controller.ts
+++ backend/src/controllers/artist.controller.ts
@@ -15,6 +15,9 @@ const upload = createMulter(eventImagePath);
 
 export class ArtistController extends BaseController {
 
+    lastRequest = new Date();
+    timeTreshold = 1000; // Musicbrainz API has limit of maximum 1 request per second. Take 0.5s margin
+
 	constructor() {
 		super("/artists");
 	}
@@ -44,14 +47,19 @@ export class ArtistController extends BaseController {
     async getArtist(req: express.Request, res: express.Response) {
         console.log("Received request to lookup artist information");
         const artist = await Artist.findByPk(req.params.artistID);
+        const currentTime = new Date();
         if (artist != null) {
-            res.status(200).json(artist);
-        } else {
+           res.status(200).json(artist);
+        } else if ((currentTime.getTime() - this.lastRequest.getTime()) < this.timeTreshold) {
+            res.status(400).json({ success: false, error: "Too many requests!"});
+        }
+        else {
             const musicBrainzApiUrl = `http://musicbrainz.org/ws/2/artist/${req.params.artistID}`;
+            this.lastRequest = new Date();
             const response = await axios.get(musicBrainzApiUrl);
             const data = await response.data
             const artist = await CreateArtist(data.name, data.id, data.type);
-            res.status(200).json(artist);
+            res.status(200).json(data);
         }
     }
 }
