88a3e38e8f3aadd6b78db7ce6ba86971e85f4df2
reinoutcl <93149166+reinoutcl@users.noreply.github.com>
2023-12-08 23:30:21 +0100

    Update to settings page

--- frontend/src/pages/settings.tsx
+++ frontend/src/pages/settings.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Image from "next/image";
+import { User } from 'lucide-react';
 import { useEffect, useState } from "react";
 import UserSettings from "@/components/UserSettings"
 
@@ -36,6 +38,13 @@ export default function Settings() {
     description: "",
   });
 
+  function showPicture(source: string) {
+    if (source != null) {
+      return <Image src={source} width={170} height={170} alt="Profile picture of user." />;
+    }
+    return <User fill={'black'} className={styles.userPicture} width={170} height={170} />;
+  }
+
 
   useEffect(() => {
       fetch(environment.backendURL + `/profile`, {
@@ -46,6 +55,7 @@ export default function Settings() {
           return response.json();
         })
         .then((responseJSON) => {
+          console.log(responseJSON);
           setUser(responseJSON);
         });
   }, []);
@@ -59,16 +69,21 @@ export default function Settings() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
-      <div className={[styles.page, styles.settingsPage].join(" ")}>
-          <div className={styles.settingContainer}>
-            <UserSettings
-            userid={user.userID}
-            />
-          </div>
-          <div className={styles.accountOverviewContainer}>
-            account overview
-          </div>
-      </div>
+        <div className={[styles.page, styles.settingsPage].join(" ")}>
+            <div className={styles.settingContainer}>
+              <UserSettings
+              userid={user.userID}
+              />
+            </div>
+            <div className={styles.accountOverviewContainer}>
+              <div className={styles.profilePictureContainer}>
+                {showPicture(user.image)}
+              </div>
+              <div className={styles.BiographySettingsContainer}>
+                test
+              </div>
+            </div>
+        </div>
       </main>
     </>
   );
