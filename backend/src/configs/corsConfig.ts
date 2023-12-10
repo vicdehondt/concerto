a90762ec78eb609f5a8a588c770b32d4cf233595
Vic De Hondt <vic.dehondt@gmail.com>
2023-12-10 14:40:19 +0100

    Fix CORS errors ONCE AND FOR ALL

--- backend/src/configs/corsConfig.ts
+++ backend/src/configs/corsConfig.ts
@@ -11,10 +11,10 @@ if (process.env.NODE_ENV == "production") {
 const corsOptions = {
     // https://www.npmjs.com/package/cors
     origin: environment.frontendURL,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    credential: "include",
-    optionsSuccessStatus: 204
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+    credentials: true
 }
 
 export function getCorsConfiguration() {
