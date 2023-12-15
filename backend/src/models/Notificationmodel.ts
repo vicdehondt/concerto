deb76df7ae569057db1ac922acf5c715aa5c9ef4
reinoutcl <93149166+reinoutcl@users.noreply.github.com>
2023-12-15 17:36:51 +0100

    Removed SSE code

--- backend/src/models/Notificationmodel.ts
+++ backend/src/models/Notificationmodel.ts
@@ -1,7 +1,6 @@
 import { DataTypes } from 'sequelize';
 import {sequelize} from '../configs/sequelizeConfig'
 import { UserModel } from './Usermodel';
-import { newNotification, notificationEmitter } from '../configs/emitterConfig';
 
 export const NotificationObject = sequelize.define('NotificationObject', {
     ID: {
