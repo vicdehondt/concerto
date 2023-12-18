6a96139fceeb326ddac2ced97889003e4736e813
Vic De Hondt <vic.dehondt@gmail.com>
2023-12-19 00:57:10 +0100

    Add filtering for events on map

--- frontend/src/components/EventMarker.tsx
+++ frontend/src/components/EventMarker.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import { useRouter } from "next/router";
 import { Event } from "./BackendTypes";
 import { environment } from "./Environment";
+import Link from "next/link";
 
 export default function EventMarker({ event }: { event: Event }) {
+  const router = useRouter();
 
-	const router = useRouter();
-
-	async function loggedIn() {
+  async function loggedIn() {
     try {
       const response = await fetch(environment.backendURL + "/auth/status", {
         mode: "cors",
@@ -19,7 +19,7 @@ export default function EventMarker({ event }: { event: Event }) {
     }
   }
 
-	async function redirectURL(normalURL: string) {
+  async function redirectURL(normalURL: string) {
     const userLoggedIn = await loggedIn();
     if (userLoggedIn) {
       return normalURL;
@@ -27,32 +27,31 @@ export default function EventMarker({ event }: { event: Event }) {
     return `/login?from=${encodeURIComponent(normalURL)}`;
   }
 
-	async function redirectClicked(event: React.MouseEvent<HTMLDivElement, MouseEvent>, url: string) {
+  async function redirectClicked(event: React.MouseEvent<HTMLDivElement, MouseEvent>, url: string) {
     event.preventDefault();
     const newUrl = await redirectURL(url);
     router.push(newUrl);
-  };
+  }
 
   return (
     <div>
-      <div>{event.title}</div>
       <div>
-        <Image src={event.eventPicture} width={50} height={50} alt="Picture of the event" />
+        <Image
+          style={{ objectFit: "cover" }}
+          src={event.eventPicture}
+          width={50}
+          height={50}
+          alt="Picture of the event"
+        />
       </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "#0B2027",
-          borderRadius: "5px",
-          width: 90,
-        }}
-      >
-        <div style={{ color: "white" }} onClick={(clickEvent) => redirectClicked(clickEvent, `/concerts/${event.eventID}`)}>
-          {" "}
-          Go to event{" "}
-        </div>
+      <div>
+        {event.title}
+        {new Date(event.dateAndTime).toLocaleString("en-US", {
+          day: "numeric",
+          month: "long",
+          year: "numeric",
+        })}
+        <Link href={`/concerts/${event.eventID}`}>Go to event</Link>
       </div>
     </div>
   );
