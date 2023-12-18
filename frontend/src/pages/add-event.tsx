bfa5034921fe6c6e00dea7bad3037c88667af77e
Vic De Hondt <vic.dehondt@gmail.com>
2023-12-19 00:57:42 +0100

    Add dynamic changing of price

--- frontend/src/pages/add-event.tsx
+++ frontend/src/pages/add-event.tsx
@@ -28,6 +28,7 @@ export default function AddEvent() {
   const [location, setLocation] = useState({venueID: "123", venueName: "Not selected"});
   const [time, setTime] = useState("")
   const [date, setDate] = useState(getFormattedDate(new Date()))
+  const [price, setPrice] = useState("");
   const [selectedArtist, setSelectedArtist] = useState<Artist | null>(null)
 
   function concatDateAndTime() {
@@ -92,7 +93,7 @@ export default function AddEvent() {
               </div>
             </div>
             <div className={styles.cardPreview}>
-              <EventCardUpload title={title} location={location.venueName} date={date} time={time} price={20} />
+              <EventCardUpload title={title} location={location.venueName} date={date} time={time} price={price as unknown as number} />
             </div>
           </div>
           <div className={styles.artistAndLocationContainer}>
@@ -101,7 +102,7 @@ export default function AddEvent() {
           <div className={styles.priceContainer}>
             Tickets
             <div className={styles.priceInput}>
-              <input type="number" name="price" id="price" placeholder="Minimum price" required />
+              <input type="number" name="price" id="price" placeholder="Minimum price" onChange={(event) => setPrice(event.target.value)} required />
               EUR
             </div>
             <div className={styles.ticketURL}>
